121c4f60458cabcca1611ef88c8a12bf61e694d7
Alyssa Vacheron <vacheron.alyssa@aol.com>
2026-04-06 17:16:57 +0200

    feat: ajoute le composant QuizList avec la logique de chargement des quiz, les templates et les styles associés

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { QuizList } from './components/quiz-list/quiz-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		component: QuizList,
+	},
+];
--- src/app/components/quiz-list/quiz-list.ts
+++ src/app/components/quiz-list/quiz-list.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, inject, signal } from '@angular/core';
+import { Quiz } from '../../models';
+import { QuizService } from '../../services/quiz';
 
 @Component({
   selector: 'app-quiz-list',
   imports: [],
   templateUrl: './quiz-list.html',
   styleUrl: './quiz-list.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class QuizList {}
+export class QuizList implements OnInit {
+  private readonly quizService = inject(QuizService);
+
+  protected readonly quizzes = signal<Quiz[]>([]);
+  protected readonly loading = signal(false);
+  protected readonly error = signal('');
+
+  ngOnInit(): void {
+    this.loadQuizzes();
+  }
+
+  protected loadQuizzes(): void {
+    this.loading.set(true);
+    this.error.set('');
+
+    this.quizService.getAllQuizzes().subscribe({
+      next: (quizzes) => {
+        this.quizzes.set(quizzes);
+        this.loading.set(false);
+      },
+      error: () => {
+        this.error.set('Impossible de charger les quiz. Vérifiez que le backend est bien démarré.');
+        this.loading.set(false);
+      },
+    });
+  }
+}
